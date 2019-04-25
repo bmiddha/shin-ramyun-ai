f682679abcc20c81e9291f91f705c859bc01a940
Dane Zieman <dane8373@gmail.com>
2019-04-25 14:33:45 -0500

    changed the master code

--- shin_ramyun_ai_master.ino
+++ shin_ramyun_ai_master.ino
@@ -1,8 +1,10 @@
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
 RF24 radio(7, 8);
 const byte address[6] = "00001";
+int currentDispenser = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -18,5 +20,32 @@ void loop() {
   if (radio.available()) {
     int data;
     radio.read(&data, sizeof(data));
+    Serial.println(String(data) + "," + String((currentDispenser+'x')));
+    while (true) {
+        while (!Serial.available()) {}
+        while (radio.available()) {
+            int garbage = 0;
+            radio.read(&garbage, sizeof(data));
+        }
+        char resp = Serial.read();
+        if (resp == 'z') {
+            break;
+        }
+    }
+    int nextCol = random(6) + 'a';
+    currentDispenser = 1;
+    Serial.println(String(data) + "," + String((currentDispenser+'x')));
+    while (true) {
+        while (!Serial.available()) {}
+        while (radio.available()) {
+            int garbage = 0;
+            radio.read(&garbage, sizeof(data));
+        }
+        char resp = Serial.read();
+        if (resp == 'z') {
+            break;
+        }
+    }
+    currentDispenser = 0;
   }
 }
