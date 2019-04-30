1f77b578812a7a475e4bfb20824515e81701d678
Bharat Middha <bmiddha99@gmail.com>
2019-04-30 17:02:47 -0500

    Update shin_ramyun_ai_motors.ino

--- shin_ramyun_ai_motors.ino
+++ shin_ramyun_ai_motors.ino
@@ -12,10 +12,12 @@ AccelStepper column2(HALFSTEP, 26, 28, 27, 29);
 AccelStepper column3(HALFSTEP, 30, 32, 31, 33);
 AccelStepper column4(HALFSTEP, 34, 36, 35, 37);
 AccelStepper column5(HALFSTEP, 38, 40, 39, 41);
-AccelStepper column6(HALFSTEP, 42, 44, 42, 45);
+AccelStepper column6(HALFSTEP, 42, 44, 43, 45);
 AccelStepper column7(HALFSTEP, 46, 48, 47, 49);
 AccelStepper dispenser1(HALFSTEP, 2, 4, 3, 5);
 AccelStepper dispenser2(HALFSTEP, 8, 10, 9, 11);
+
+byte direction[7] = {1, 1, -1, -1, 1, 1, -1};
 
 AccelStepper *columns[NUM_COLUMNS] = {
   &column1,
@@ -46,32 +48,33 @@ void initSteppers() {
   }
 }
 
-void openColumn(AccelStepper *s, char disp) {
-  s->move(512);
+void openColumn(AccelStepper *s, int dir, char disp) {
+  s->move((dir) * (512));
   s->runToPosition();
   if (disp >= 'x' && disp <= 'y') openDispenser(dispensers[(int)(disp - 'x')]);
-  delay(7000);
-  s->move(-512);
+  delay(5000);
+  s->move((dir) * (-512));
   s->runToPosition();
   Serial.println("Z");
 }
 
 void openDispenser(AccelStepper *s) {
-  s->move(512);
+  s->move(-512);
   s->runToPosition();
 }
 
 void setup() {
   Serial.begin(115200);
+  Serial.begin(115200);
   initSteppers();
 }
 
 void loop() {
-  while (!Serial.available()) {}
   String input;
   input = Serial.readString();
   char col = input[0];
   char disp = input[2];
-  if (col >= 'a' && col <= 'g') openColumn(columns[col - 'a'], disp);
 
+  if (col >= 'a' && col <= 'g')
+    openColumn(columns[col - 'a'], direction[col - 'a'], disp);
 }
